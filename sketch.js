5963eb55e72e9f495a6b3f33c3ab58e53a814d53
Y44sQu33n <94427572+Y44sQu33n@users.noreply.github.com>
2021-12-06 18:05:31 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+
+function setup() {
+  createCanvas(400,400);
+  background(51);
+  box = createSprite(200,200,30,30);
+
+}
+
+function draw() 
+{
+
+  if (keyIsDown(RIGHT_ARROW))
+  {
+    background("purple")
+
+  }
+  
+  
+
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("blue");
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("pink");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
+  }
+
+
+  
+  drawSprites();
+}
+
